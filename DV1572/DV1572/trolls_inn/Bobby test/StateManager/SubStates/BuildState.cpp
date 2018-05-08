271157c9a89186969af685f1b1e52ea15b38b927
Joakim <joakim.trossvik@gmail.com>
2018-05-08 15:19:56 +0200

    QuickFix

--- DV1572/DV1572/trolls_inn/Bobby test/StateManager/SubStates/BuildState.cpp
+++ DV1572/DV1572/trolls_inn/Bobby test/StateManager/SubStates/BuildState.cpp
@@ -532,7 +532,7 @@ void BuildState::_inputFurniture()
 		else if (m_startTile)
 		{
 			DirectX::XMFLOAT3 p(m_startTile->getPosition());
-			table->setPosition(p.x + 0.5f, p.y - 0.2f, p.z + 0.5f);
+			table->setPosition(p.x , p.y - 0.2f, p.z);
 			DirectX::XMINT2 start;
 			start.x = table->getPosition().x;
 			start.y = table->getPosition().z;
@@ -592,7 +592,7 @@ BuildState::BuildState(Camera * cam,
 
 	//TEMP
 	door.setMesh(MeshHandler::getDoor());
-	table = new Table(DirectX::XMFLOAT3(0, 0, 0), MeshHandler::getTable());
+	table = new Bed(DirectX::XMFLOAT3(0, 0, 0), nullptr);
 }
 
 BuildState::~BuildState()
--- DV1572/DV1572/trolls_inn/Furniture/Table.cpp
+++ DV1572/DV1572/trolls_inn/Furniture/Table.cpp
@@ -1,11 +1,17 @@
 #include "Table.h"
+#include "../Mesh Manager/MeshManager.h"
 
 Table::Table(DirectX::XMFLOAT3 pos, Mesh * mesh, const int setting) : Furniture(pos, mesh)
 {
 	p_gridSize = 1;
 	p_rot = 0;
-
-	p_object.setMesh(mesh);
+	if (!mesh)
+	{
+		p_object.setMesh(MeshHandler::getTable());
+	}
+	else {
+		p_object.setMesh(mesh);
+	}
 	p_object.setPos(pos.x + 0.5f, pos.y - 0.2f, pos.z + 0.5f);
 	//Set Whatever suits
 	//p_attributes
